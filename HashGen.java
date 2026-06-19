5e718699a8daab82df182d2668058dfac53c29a3
JohnPham666 <khaitrieu6652@gmail.com>
2026-06-19 13:22:20 +0700

    Cleanup: remove duplicate/stale files - driver/assets old codebase, staff duplicates, test JSONs, v7 SQL, HashGen.java

--- HashGen.java
+++
@@ -1,9 +0,0 @@
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-public class HashGen {
-    public static void main(String[] args) {
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        System.out.println("admin123 : " + encoder.encode("admin123"));
-        System.out.println("staff123 : " + encoder.encode("staff123"));
-    }
-}
