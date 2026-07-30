ff8465c49a6e5e3300c5b02ee896ef88064f504e
JohnPham666 <khaitrieu6652@gmail.com>
2026-07-31 00:02:24 +0700

    fix: import HttpServletRequest in VnPayService

--- src/main/java/com/parking/management/module/payment/VnPayService.java
+++ src/main/java/com/parking/management/module/payment/VnPayService.java
@@ -11,6 +11,7 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
+import jakarta.servlet.http.HttpServletRequest;
 
 @Service
 public class VnPayService {
